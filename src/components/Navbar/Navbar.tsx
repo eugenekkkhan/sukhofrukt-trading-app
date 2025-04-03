301396042e3b9355153c990e57856d8fd364da22
eugenekkkhan <eugenekhananov@yandex.ru>
2025-04-03 09:33:34 +0300

    refactor Navbar component to improve responsiveness and add icon links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import { useLocation } from "react-router";
 import ThemeSwitcher from "../ThemeSwitcher";
+import { RxDashboard } from "react-icons/rx";
+import ButtonWithIcon from "../Buttons/ButtonWithIcon";
+import { SlSettings } from "react-icons/sl";
+import { MdHistory } from "react-icons/md";
+import { GoHistory } from "react-icons/go";
 
 const Navbar = () => {
   const [websiteName, setWebsiteName] = useState("Сухофрукт Trading Bot");
@@ -9,22 +14,18 @@ const Navbar = () => {
   const navRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (window.innerWidth < 500) {
-      setWebsiteName("СTB");
-    } else if (window.innerWidth < 640) {
-      setWebsiteName("СTradingBot");
+    if (window.innerWidth < 720) {
+      setWebsiteName("СУХОФРУКТ");
     } else {
       setWebsiteName("Сухофрукт Trading Bot");
     }
-    // window.addEventListener('resize', ()=>{
-    //   if (window.innerWidth < 500) {
-    //     setWebsiteName("СTB");
-    //   } else if(window.innerWidth < 640){
-    //     setWebsiteName("СTradingBot");
-    //   } else {
-    //     setWebsiteName("Сухофрукт Trading Bot");
-    //   }
-    // })
+    window.addEventListener('resize', ()=>{
+      if(window.innerWidth < 720){
+        setWebsiteName("СУХОФРУКТ");
+      } else {
+        setWebsiteName("Сухофрукт Trading Bot");
+      }
+    })
     setTimeout(() => {
       if (navRef.current)
         navRef.current.style.transition =
@@ -36,20 +37,55 @@ const Navbar = () => {
     <div className="nav bottom-border" ref={navRef}>
       <p className="medium">{websiteName}</p>
       <div className="nav-links">
-        <a
-          href="/"
-          className={location.pathname === "/" ? "medium green" : "green"}
-        >
-          Dashboard
-        </a>
-        <a
-          href="/settings"
-          className={
-            location.pathname === "/settings" ? "medium green" : "green"
-          }
-        >
-          Настройки
-        </a>
+        {/* Text */}
+        <div className="nav-links-text">
+          <a
+            href="/"
+            className={location.pathname === "/" ? "medium green" : "green"}
+          >
+            Dashboard
+          </a>
+          <a
+            href="/settings"
+            className={
+              location.pathname === "/settings" ? "medium green" : "green"
+            }
+          >
+            Настройки
+          </a>
+          <a
+            href="/history"
+            className={
+              location.pathname === "/history" ? "medium green" : "green"
+            }
+          >
+            История
+          </a>
+        </div>
+        {/* Icons */}
+        <div className="nav-links-icons">
+          <a href={location.pathname === "/" ? undefined : "/"}>
+            <RxDashboard 
+              cursor='pointer'
+              size={28}
+              className={location.pathname === "/" ? "green" : "green-light"} 
+            />
+          </a>
+          <a href={location.pathname === "/settings" ? undefined : "/settings"}>
+            <SlSettings 
+              cursor='pointer'
+              size={28}
+              className={location.pathname === "/settings" ? "green" : "green-light"} 
+            />
+          </a>
+          <a href={location.pathname === "/history" ? undefined : "/history"}>
+            <GoHistory 
+              cursor='pointer'
+              size={28} 
+              className={location.pathname === "/history" ? "green" : "green-light"} 
+            />
+          </a>
+        </div>
         <ThemeSwitcher />
       </div>
     </div>
